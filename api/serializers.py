0e7de90c6196acbd99d4c4de42185e34d31ef992
bay007 <tnt.galicia@gmail.com>
2017-09-03 22:27:50 -0500

    Agregar los otros objetos a la API

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from adopcion.models import Persona
+from mascota.models import Mascota, Vacuna
 
 
 class PersonaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Persona
+        fields = '__all__'
+
+
+class MascotaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mascota
+        fields = '__all__'
+
+
+class VacunaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vacuna
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from api import serializers
 from rest_framework import viewsets
 from adopcion.models import Persona
+from mascota.models import Mascota, Vacuna
 
 # Create your views here.
 
@@ -8,3 +9,13 @@ from adopcion.models import Persona
 class PersonaViewSet(viewsets.ModelViewSet):
     queryset = Persona.objects.all()
     serializer_class = serializers.PersonaSerializer
+
+
+class MascotaViewSet(viewsets.ModelViewSet):
+    queryset = Mascota.objects.all()
+    serializer_class = serializers.MascotaSerializer
+
+
+class VacunaViewSet(viewsets.ModelViewSet):
+    queryset = Vacuna.objects.all()
+    serializer_class = serializers.VacunaSerializer
